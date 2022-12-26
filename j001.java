0fd553acaea10eb3f79f60f0d619e01604d10580
Yasuhiro <taihong@maranic.net>
2022-12-26 16:01:47 +0900

    2022年12月26日　変更

--- j001.java
+++ j001.java
@@ -3,13 +3,13 @@
 */
 class j001
 {
-	public static void main (String[] args)
-	{
-		int sum = 0;
-		for(int i =0; i <= 10 ; i++ ) {
-		    sum = sum + i;
-		}
-		
-		System.out.println(sum);
-	}
+    public static void main (String[] args)
+    {
+        int sum = 0;
+        for(int i =0; i <= 10 ; i++ ) {
+            sum = sum + i;
+        }
+        
+        System.out.println(sum);
+    }
 }
